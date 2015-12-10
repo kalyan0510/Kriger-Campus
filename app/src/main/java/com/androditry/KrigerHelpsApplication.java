b95a47788e951eac938fc171781ac47b94396fdb
krigercampus <krigercampus@gmail.com>
2015-12-10 17:09:33 +0530

    Change of Keys

--- app/src/main/java/com/androditry/KrigerHelpsApplication.java
+++ app/src/main/java/com/androditry/KrigerHelpsApplication.java
@@ -8,14 +8,15 @@ import android.app.Application;
 public class KrigerHelpsApplication extends Application{
 
 	// Krigermotivation keys
-	/*static final String APPLICATION_ID="vYqrSskYroBI5OPi8NUQpF2ke4fpNRaJgj8Slab5";
-	static final String CLIENT_KEY="rVrswITftDTPHZxaIlBeekPgaycUdRn6My6yIpL6"; */
+	static final String APPLICATION_ID="vYqrSskYroBI5OPi8NUQpF2ke4fpNRaJgj8Slab5";
+	static final String CLIENT_KEY="rVrswITftDTPHZxaIlBeekPgaycUdRn6My6yIpL6";
 
 
 	//Krigercampus keys
+	/*
 	static final String APPLICATION_ID="odIPfeae9qYSoQMg0qMXtzfcWkXLFNNdzK2dhoiv";
 	static final String CLIENT_KEY="iSIT7mJCmZFkxHvUEpk6QlXxolWxze5hBVjcnpDr";
-	
+	*/
 	@Override
 	public void onCreate() {
 	    super.onCreate();
